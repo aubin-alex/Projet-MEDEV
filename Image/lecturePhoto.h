21ac8b817e2320ef81835965d8bb66704292f78d
aubin-alex <aubin.rolin@eleves.ec-nantes.fr>
2015-11-26 10:33:26 +0100

    Header pour nos fonctions

---
+++ Image/lecturePhoto.h
@@ -0,0 +1,14 @@
+/* 
+ * File:   lecturePhoto.h
+ * Author: eleve
+ *
+ * Created on 26 novembre 2015, 10:32
+ */
+
+#ifndef LECTUREPHOTO_H
+#define	LECTUREPHOTO_H
+
+
+
+#endif	/* LECTUREPHOTO_H */
+
