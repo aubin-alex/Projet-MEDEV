8b45d053c865b1afe21c57a6efa44f1d26c28785
aubin-alex <aubin.rolin@eleves.ec-nantes.fr>
2015-11-26 10:28:05 +0100

    Voila le fichier main, c'est parti pour l'aventure

---
+++ Image/main.cpp
@@ -0,0 +1,19 @@
+/* 
+ * File:   main.cpp
+ * Author: eleve
+ *
+ * Created on 26 novembre 2015, 10:08
+ */
+
+#include <cstdlib>
+
+using namespace std;
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    return 0;
+}
+
